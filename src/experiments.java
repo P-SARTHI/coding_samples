ac3463c6be389b95137042d3f6f14ef085578fee
P-SARTHI <parthsarthiteotia@gmail.com>
2023-12-08 22:53:32 +0530

    rotten orange update

--- src/experiments.java
+++ src/experiments.java
@@ -3,7 +3,7 @@ import java.util.HashMap;
 
 class experiments{
    public static void main(String args[]){
-    int arr[]={ 10, 2, -2, -20, 10};
+    int arr[][]={ {0}};
     int k=-100;
      System.out.println((char)('a'+1));
    }
--- src/rotten_orange_problem.java
+++ src/rotten_orange_problem.java
@@ -1,50 +1,11 @@
 import java.util.*;
 
 public class rotten_orange_problem {
+     
     public static void main(String args[]){
        
+      int  grid[][]={{2,1,1},{0,1,1},{1,0,1}};
+      
+     
     }
-    public static void orangeRotting(int grid[][]){
-        class Node{
-               int row;
-               int columb;
-               Node(int i,int j){
-                this.row=row;
-                this.columb=columb;
-               }
-        }
-        int rows=grid.length;
-        int colmbs=grid[0].length;
-        Node start=new Node(0, 0);
-        int vertices=0;
-        HashSet<Node> blockade=new HashSet<>();
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<colmbs;j++){
-                if(grid[i][j]==2){
-                  start=new Node(i, j);
-                  
-                } 
-                if(grid[i][j]==0){
-                  blockade.add(new Node(i,j));
-                }
-            }
-        }
-      boolean visited[][]=new boolean[grid.length][grid[0].length];
-      Queue<Node> qu=new LinkedList<>();
-      qu.add(new Node(start.row, start.columb));
-      visited[start.row][start.columb]=true;
-      while(!qu.isEmpty()){
-
-      }
-    }
-    public static boolean inbounds(int row,int columb,int grid[][]){
-          if((row>=0 && row<=grid.length-1) && (columb>=0 && columb<=grid[0].length-1)){
-            return true;
-          }
-          else{
-            return false;
-          }
-    }
-    
 }
-
